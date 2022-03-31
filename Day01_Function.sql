fc749a04f7025be671fd56c057fb8ffaa43e68c9
Leehojun12 <dlghwns82@naver.com>
2022-03-31 15:53:41 +0900

    Update

--- Day01_Function.sql
+++ Day01_Function.sql
@@ -82,9 +82,173 @@ select replace('Hello Hi High','Hi','') from dual;
 select abs(-10) from dual;
 
 -- mod(숫자, 나눌 값) : 인자로 전달받은 숫자를 나눌 값으로 나머지를 반환해주는 함수
-select mod(10, 3) from dual;
+select mod(10, 4) from dual;
+-- round(숫자) : 인자로 받은 숫자를 반올림하여 반환해주는 함수
+select round(123.678) from dual;
+-- round(숫자, 소숫점 인덱스 위치) : 인자로 받은 숫자를 지정한 위치 다음 자리수에서 반올림하여 반환해주는 함수
+select round(123.456, 2) from dual;
+-- floor(숫자) : 인자로 받은 숫자의 소수점 자리를 모두 버리는 함수
+select floor(123.455123) from dual;
+-- trunc(숫자, 소수점위치) : 인자로 받은 숫자를 지정한 위치까지 잘라서 반환해주는 함수
+select trunc(123.345, 2) from dual;
+-- ceil(숫자) : 인자로 받은 숫자의 소수점 자리를 버리고 반환해주는 함수(올림)
+select ceil(123.345) from dual;
 
+-- employee 테이블에서 직원명, 입사일, 오늘까지의 근무일수 조회(주말도 포함, 소수점아래는 버림)
+select * from employee;
+select emp_name"직원명", hire_date"입사일", floor(sysdate-hire_date)"근무일수" from employee;
+=======================================================================================================================
+-- 날짜 함수
+-- sysdate : 시스템에 저장되어있는 현재 날짜(시간 포함) 반환
+select sysdate from dual;
+-- current_date : session(접속) timezone에 따라 현재 날짜(시간) 반환
+select current_date from dual;
+-- months_between(날짜형, 날짜형) : 두 날짜 사이의 개월 수 차이를 반환해주는 함수
+select floor(months_between(sysdate, hire_date)/12) from employee;
+select floor(months_between(sysdate, sysdate+32)) from employee;
+-- add_months(기준 날짜, 더할 개월 수) : 첫 번째 인자 값인 날짜를 기준으로 해서 두 번째 인자값을 더해서 반환해주는 함수
+select add_months(sysdate, -1) from dual;
+select emp_name, hire_date, add_months(hire_date,6) from employee;
+-- next_day(기준 날짜, 요일/숫자) : 기준 날짜를 기준으로 오른쪽에 해당하는 가장 가까운 날짜를 반환해주는 함수
+-- 요일의 형식 : '월' 혹은'월요일'
+-- 1~7 -> 일~토
+select next_day(sysdate, 1) from dual;
 
+-- last_day(기준 날짜) : 기준 날짜를 바탕으로 해당 날짜가 속한 달의 마지막 날을 반환해주는 함수
+select last_day(add_months(sysdate,1)) from dual;
+select emp_name, hire_date, last_day(add_months(sysdate,1)) from employee;
 
+-- extract(year/month/day from date(기준날짜)) : 기준날짜로부터 년/월/일을 추출해서 반환해주는 함수
+select extract(year from sysdate) from dual;
+select extract(month from sysdate) from dual;
+select extract(day from sysdate) from dual;
+=======================================================================================================================
+-- 문제
+-- 입사년도 YYYY년 MM월DD일 표현
+-- 년차는 올림
+-- 
+select * from employee;
+select emp_name, extract(year from hire_date)||'년'
+    || extract(month from hire_date)||'월'
+    || extract(day from hire_date)||'일' "입사년도",
+    ceil((sysdate-hire_date)/365)||'년차' "년차"
+    from employee
+     order by 3;
+-- 문자열을 기준으로 order by -> 문자로써 더 작은 숫자를 작다고 인식 (10과 3이 있다면 10이 더 작은수)
 
+-- 특별 보너스를 지급 -> 자료가 필요 
+-- employee -> 입사일을 기준으로 다음달 1일부터 6개월 계산해서 조회
+-- 사워명, 입사일, 기준일, 기준일 + 6개월
+-- 22/01/05 입사 -> 22/02/01 기준일 -> 22/08/01 기준일+ 6개월
+-- 출력시 입사일 기준 출력
+select emp_name "사원명", hire_date"입사일",last_day(add_months(hire_date,6))+1"기준일+6" from employee order by 2;
+=======================================================================================================================
+-- 형변환
+-- to_char(date/number, format) : 날짜 혹은 숫자를 특정한 형식의 문자형으로 반환해주는 함수
+select sysdate from dual;
+select to_char(sysdate, 'YYYY-MM-DD') from dual;
 
+select emp_name "사원명", 
+    to_char(hire_date,'YYYY"년"MM"월"DD"일"')"입사년월일" 
+from employee;
+
+--day : x요일
+select to_char(sysdate, 'YY/MM/DD/DAY') from dual;
+--dy : x(요일)
+select to_char(sysdate, 'YY/MM/DD/DY') from dual;
+-- MONTH : x월
+select to_char(sysdate, 'YY/MONTH/DD/DY') from dual;
+-- HH / MI / SS 시분초
+select to_char(sysdate, 'HH:MI:SS') from dual;
+-- PM/AM : 오후 / 오전
+select to_char(sysdate, 'PMHH:MI:SS') from dual;
+--HH24 : 24시간 기준 표기
+select to_char(sysdate, 'HH24:MI:SS') from dual;
+
+--employee 데이블 사원명, 입사일 -> 1990/02/05 화요일 형태 조회
+select emp_name"사원명", 
+    to_char(hire_date,'YYYY/MM/DD/(DY)')"입사일" 
+from employee;
+=======================================================================================================================
+-- 숫자 -> 문자형
+-- to_char(대상 숫자, )
+-- 123456789 -> 0이나 9를 이용해 문자형으로 변환
+select to_char(123456789, '000,000,000') from dual;
+select to_char(12345, '999,999,999') "9",
+    to_char(12345, '000,000,000') "0"
+from dual;
+
+-- 통(원)화 표시 -> L (local)
+-- nvl(숫자/컬럼, 치환 값) : 만약 해당 숫자/컬럼 등이 null 값이라면 치환값으로 변환해주는 함수
+select to_char(12345, 'L999,999') from dual;
+-- employee -> 직원명, 직급코드, 연봉(원) 조회
+-- 단, 연봉은  ￦12,345
+-- 연봉 보너스가 적용된 금액으로 조회((월급*bonus)*12)
+select emp_name "직원명",
+    job_code"직급코드",
+    to_char(salary,'999,999,999') "월급(원)",    
+    to_char((salary + (salary*nvl(bonus, 0))*12),'L999,999,999')"연봉(원)*보너스" 
+from employee;
+=======================================================================================================================
+-- to_date(숫자/문자, 형식) : 숫자형 혹은 문자형 데이터를 데이트(날짜) 타입으로 바꿔주는 함수
+-- 20100505
+select to_date(20120505, 'YYYYMMDD') from dual;
+select to_date('20120505', 'YYYYMMDD') from dual;
+-- 시간값을 변환할때 앞에 년/월/일 정보를 주지 않으면 현재 달의 1일로 날짜를 인식해버림.
+select to_char(to_date('110331', 'hh:mi:ss'), 'YYYY/MM/DD hh:MI:SS') from dual;
+-- 년도 값을 변환할때 2000/1900년대에 대한 값을 명확히 제시하지 않으면 2000년대를 default 값으로 인식
+select to_char(to_date('890909', 'YY/MM/DD'),'YYYY/MM/DD') from dual;
+select emp_name "사원명",
+    emp_no "사번",
+    hire_date "입사일"
+from employee
+    where hire_date >= to_date('20000101','YYYYMMDD');
+=======================================================================================================================
+-- to_number(문자형, 형식) : 문자형을 숫자형으로 변환해주는 함수
+select to_number('123,456,789', '999,999,999') from dual;
+select to_number('123,456,789', '999,999') from dual; -- 넘겨주는 문자형식에 맞춰 숫자형식도 동일하게.
+select to_number('s23,456', '999,999') from dual; -- 숫자로 변환불가한 문자열을 넘겨줄 수 X
+
+-- 묵시적 형변환 : 오라클에서는 자동적으로 자료형을 유추해서 형변환
+select emp_name, salary from employee
+    where salary = 8000000;
+select 25 + '25' from dual;
+select emp_name, hire_date from employee
+    where hire_date = to_date('20130206', 'YY/MM/DD');
+    
+/*
+60년생의 직원명과 년생, 보너스 율을 출력
+그때 보너스 값이 null인 경우에는 0% 라고 출력
+*/
+select emp_name "직원명",
+    substr(emp_no,1,2)"년생",
+    nvl(bonus,0)*100||'%'"보너스"
+from employee
+    where substr(emp_no,1,2) between 60 and 69;
+=======================================================================================================================
+-- 그룹 함수
+-- sum(숫자/컬럼) : 해당 컬럼/ 숫자 값의 총 합을 구해주는 함수
+-- 그룹함수를 사용할때에는 단일한 결과값이 나온다는 점
+select dept_code, sum(salary) from employee
+    where dept_code = 'D5';
+select * from employee;
+-- avg(컬럼) : 해당 컬럼 값들의 평균을 구해주는 함수
+select round(avg(salary)) from employee;
+-- 전 직원의 보너스 평균 -> 소수점 셋째자리수에서 반올림해서 조회
+select round(avg(nvl(bonus,0)),2)"보너스" from employee;
+
+--count(컬럼) : 해당 컬럼 -> 데이터의 개수를 반환해주는 함수
+select count(*) from employee;
+-- bonus를 지급해야하는 사원의 수 카운트
+select count(*) from employee
+    where bonus is not null;
+-- 부서코드가 D5인 사원의 수 카운트
+select count(*) from employee
+    where dept_code = 'D5';
+-- 사원들이 속해 있는 부서의 수 카운트
+select count(distinct dept_code) from employee;
+
+-- max/min(컬럼) : 해당 그룹에서 최대값/ 최솟값을 반환해주는 함수
+select max(salary), min(salary) from employee;
+--employee 테이블에서 가장 오래 일한 사원의 입사일 / 가장 적게 일한 사원의 입사일 조회
+select min(hire_date), max(hire_date) from employee;
